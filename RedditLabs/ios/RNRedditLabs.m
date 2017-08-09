4a15e64b6d7410aa0273908899cfa65c902daa4e
garrettmac <garrett@vyga.io>
2017-08-09 10:24:40 -0600

    80 file(s) changed: (on Wed Aug  9 10:24:40 MDT 2017)

--- RedditLabs/ios/RNRedditLabs.m
+++ RedditLabs/ios/RNRedditLabs.m
@@ -8,6 +8,30 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+//----------------------------------------
+/*RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}*/
+RCT_EXPORT_METHOD(addEvent:(NSString *)name details:(NSDictionary *)details)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+  NSString *location = [RCTConvert NSString:details[@"location"]];
+  NSDate *time = [RCTConvert NSDate:details[@"time"]];
 
+}
+
+//----------------------------------------
+/*- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"EventReminder"];
+}
+
+- (void)calendarEventReminderReceived:(NSNotification *)notification
+{
+  NSString *eventName = notification.userInfo[@"name"];
+  [self sendEventWithName:@"EventReminder" body:@{@"name": eventName}];
+}*/
+
+//----------------------------------------
 @end
-  
--- RedditLabsDemo/index.ios.js
+++ RedditLabsDemo/index.ios.js
@@ -8,24 +8,34 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
+  Text,TouchableOpacity,
   View
 } from 'react-native';
-
+import RNRedditLabs from 'react-native-redditlabs';
+console.log(" RNRedditLabs: ",RNRedditLabs);
 export default class RedditLabsDemo extends Component {
+
+  redditLabs(){
+var date = new Date();
+    RNRedditLabs.addEvent('Birthday Party', {
+  location: '4 Privet Drive, Surrey',
+  time: date.getTime(),
+  description: '...'
+})
+console.warn("done");
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableOpacity onPress={this.redditLabs}>
+    <Text style={{color:'rgba(0,0,0,0.5)',fontSize: 20,textAlign: "center",margin: 10,}}>
+  redditLabs
+    </Text>
+  </TouchableOpacity>
       </View>
     );
   }
